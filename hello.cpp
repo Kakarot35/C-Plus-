e03099cb5da688f58cff3bda0aefde16008f752e
Kakarot35 <mhatrekaran989@gmail.com>
2026-06-29 22:41:22 +0530

    Feat: C++

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int num1 = INT_MIN;
+    int num2 = INT_MAX;
+    cout << "Minimum :"<< INT_MIN << endl;
+    cout <<"Maxmimum :"<< INT_MAX << endl;
+    return 0;
+}
