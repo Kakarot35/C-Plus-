f3899c3e0a555e57c597d2609d5221c6e139808d
Kakarot35 <mhatrekaran989@gmail.com>
2026-06-30 13:47:44 +0530

    Feat: Loops Practice

---
+++ loop1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i;
+
+    i = 5;
+    while(i <= 50 ){
+        cout << "number = " << i << endl;
+
+        i = i + 5 ;
+    }
+}
