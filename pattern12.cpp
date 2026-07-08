a4f7fe063bc520c66ddd3fd94d04ce778acd46cf
Kakarot35 <mhatrekaran989@gmail.com>
2026-07-08 14:07:47 +0530

    feat:patttern

---
+++ pattern12.cpp
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+
+
+        for(char ch = 'A' + n -i; ch <= 'A' + n -1; ch++){
+            cout << ch << " ";
+        }
+        cout << '\n';
+    }
+}
