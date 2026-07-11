1b02a34ab1986629c6bc781af4edfabdc3adf9b0
Kakarot35 <mhatrekaran989@gmail.com>
2026-07-11 23:11:06 +0530

    feat : Last

---
+++ pattern16.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void pattern22(int n) {
+
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int depth = min({top, left, bottom, right});
+
+                cout << n - depth << " ";
+            }
+
+            cout << '\n';
+        }
+    }
+};
