31be82e75528cac4d1ddc8b4f7b64a23dcb612d7
Kakarot35 <mhatrekaran989@gmail.com>
2026-07-20 15:45:02 +0530

    Feat: Reverse of array

---
+++ Array Basics/Reverse.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[]= {5,4,3,2,1};
+    int n = 5;
+
+    int reverse;
+    for(int i=n-1; i >= 0; i--){
+        reverse = arr[i];
+        cout << reverse << " ";
+    }
+    
+}
