e4de6ab353dd31f551a63bde104516f6eb3c1963
Kakarot35 <mhatrekaran989@gmail.com>
2026-07-23 23:03:32 +0530

    Feat

--- Array Basics/Sum.cpp
+++ Array Basics/Sum.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-    int arr[] = {1,2,3,4,5,6};
-    int n = 6;
+    int arr[] = {1,2,3,4,5};
+    int n = 5;
     int sum = 0;
     for(int i = 0; i <= n-1; i++){
         sum += arr[i];
